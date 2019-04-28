cc292717b040dc1e7cb57b4618d9e79edcdcbe6c
Sophie van Duivenboden <>
2019-04-28 17:32:50 +0200

    added eject and boostrap, added components for the creatives

---
+++ src/components/Creatives/Creatives.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Creatives.scss';
+
+class Creatives extends React.Component {
+  render(){
+    return (
+      <div className="Creatives">
+        <div className="container">Test</div>
+      </div>
+    );
+  }
+}
+
+export default Creatives;
